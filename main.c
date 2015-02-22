3a41141b127f9acfdff549efe8b7a98c6c106320
April Sin <jazziblue@icloud.com>
2015-02-22 12:20:54 +0800

    first c

---
+++ main.c
@@ -0,0 +1,79 @@
+#define	StarName_BigDipper		1
+#define	StarName_Scorpius		2
+#define	StarName_Orion          3
+#define	StarName_Gemini         4
+#define	StarName_Taurus         5
+#define	StarName_Pegasus        6
+#define	StarName_CanisMajor     7
+#define	StarName_Libra          8
+
+#define	SN_A	StarName_BigDipper	
+#define	SN_B	StarName_Scorpius
+#define	SN_C	StarName_Orion
+#define	SN_D	StarName_Gemini
+#define	SN_E	StarName_Taurus
+#define	SN_F	StarName_Pegasus
+#define	SN_G	StarName_CanisMajor
+#define	SN_H	StarName_Libra
+
+#define	StarGraph_BigDipper     1
+#define	StarGraph_Scorpius		2
+#define	StarGraph_Orion         3
+#define	StarGraph_Gemini		4
+#define	StarGraph_Taurus		5
+#define	StarGraph_Pegasus       6
+#define	StarGraph_CanisMajor    7
+#define	StarGraph_Libra         8
+
+#define	StarGraph_Dummy1		11
+#define	StarGraph_Dummy2		12
+#define	StarGraph_Dummy3		13
+#define	StarGraph_Dummy4		14
+#define	StarGraph_Dummy5		15
+
+#define	SG_A	StarGraph_BigDipper
+#define	SG_B	StarGraph_Scorpius
+#define	SG_C	StarGraph_Orion
+#define	SG_D	StarGraph_Gemini
+#define	SG_E	StarGraph_Taurus
+#define	SG_F	StarGraph_Pegasus
+#define	SG_G	StarGraph_CanisMajor
+#define	SG_H	StarGraph_Libra
+
+#define	SG_J	StarGraph_Dummy1
+#define	SG_K	StarGraph_Dummy2
+#define	SG_L	StarGraph_Dummy3
+#define	SG_M	StarGraph_Dummy4
+#define	SG_N	StarGraph_Dummy5
+
+rndList[]={ 1, 7, 3, 5, 4, 6, 2, 1, 8, 5, 
+		3, 6, 2, 4, 7, 8, 1, 6, 5, 3, 
+		1, 8, 6, 4, 2, 1, 5, 3, 2, 7,
+		6, 8, 3, 1, 7, 4, 6, 2, 1, 8,
+		3, 1, 7, 2, 8, 5, 7, 3, 2, 8, 0}
+
+main()
+{
+}
+
+xxButtonIsOn(int button)
+{
+}
+
+xxSetLED(int LED, binary status)
+{
+}
+
+xxAudioPlay(int audioclip)
+{
+}
+
+xxAudioPlayBackground(int audioclip)
+{
+}
+
+xxRandom()
+{
+}
+
+
