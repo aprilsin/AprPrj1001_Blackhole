bb82705fdc132833f6b333038afc2cb2e1456215
RainbowCat <jazziblue@icloud.com>
2015-02-23 18:00:05 +0800

    Adjusting slowdown delay time and adding the use of 'starPin'

--- Test/switch001.c
+++ Test/switch001.c
@@ -16,20 +16,15 @@
 ****************************************************************************/
 #include <STC89.H>
 
+#define listPin		P14
+#define starPin		P15
+
 #define	HIGH		1
 #define LOW			0
 
 #define	EOL			0xff
 
 #define	DELAY_STD	2000
-
-char playerList;
-char playerStar;
-
-unsigned int  slowdown[]= {3500, 4000, 5000, 7500, 8000, 10000, 11000, 15000, 20000, 30000,
-						   40000, 50000, 55000, 58000, 65000, 0};
-
-void stateList(void);		// Prototype
 
 void delay(unsigned int d)
 {
@@ -39,7 +34,7 @@ void delay(unsigned int d)
 
 } /* end delay */
 
-unsigned char ledOn(unsigned char led)
+unsigned char ledOn(unsigned char led) 
 {
 	return ~led;
 
@@ -47,14 +42,14 @@ unsigned char ledOn(unsigned char led)
 
 unsigned char getRandomList(void)
 {
-	static int	rnd=0;
+	static unsigned int	rnd=0;
 		
 	unsigned char random;
 	unsigned char rndList[] = { 6, 4, 0, 7, 6, 4, 2, 7, 3, 0, 5, 1, 5, 6, 5, 4, 5, 7, 4, 1,
 								3, 4, 1, 2, 4, 0, 5, 0, 5, 7, 3, 1, 6, 3, 0, 2, 3, 5, 0, 3,
-								6, 0, 4, 3, 6, 2, 0, 1, 5, 6, 4, 0, 7, 3, 6, 2, 3, 6, 2, 0,
-								7, 0, 4, 1, 5, 0, 3, 4, 2, 5, 7, 5, 1, 7, 6, 5, 4, 6, 7, 4,
-								6, 7, 0, 1, 6, 4, 5, 6, 5, 7, 0, 4, 5, 3, 2, 5, 2, 4, 5, 7,
+//								6, 0, 4, 3, 6, 2, 0, 1, 5, 6, 4, 0, 7, 3, 6, 2, 3, 6, 2, 0,
+//								7, 0, 4, 1, 5, 0, 3, 4, 2, 5, 7, 5, 1, 7, 6, 5, 4, 6, 7, 4,
+//								6, 7, 0, 1, 6, 4, 5, 6, 5, 7, 0, 4, 5, 3, 2, 5, 2, 4, 5, 7,
 					  		    EOL};
 
 	random = rndList[rnd];
@@ -65,19 +60,14 @@ unsigned char getRandomList(void)
 
 } /* end getRandomList */
 
-void main(void)
-{
-	stateList();
-
-} /* end main */
-
 void stateList(void)
 {
 	/* Init player selection to invalid */
 	unsigned char playerList = 0xff;
-	unsigned char playerStar = 0xff;
 
-	unsigned char a;
+	static unsigned int a=0;
+	unsigned int slowdown[]= { 3500, 3600, 3800, 4200, 5000, 6600, 
+							   9800, 16200, 29000, 54600, 0};
 
 	/* Turn off all LEDs */
 	P0 = 0xff;
@@ -85,29 +75,37 @@ void stateList(void)
 	P2 = 0xff;
 
 	/* Set Pin ready for Input */
-	P14 = HIGH;
-	P15 = HIGH;
+	listPin = HIGH;
+	starPin = HIGH;
 	P16 = HIGH;
 	P17 = HIGH;
 
-    for (;;)
+	while (listPin == HIGH);		/* Key Off */
+
+	while (listPin == LOW)		   	/* Key On */
 	{
-		while (P14 == HIGH);		/* Key Off */
-
-		while (P14 == LOW)
-		{
-			P0 = ledOn(1 << getRandomList());
-			
-			delay(DELAY_STD);
-		}
-
-		a=0;
-	    while (slowdown[a]!= 0)
-		{
-			playerList = getRandomList();
-			P0 = ledOn(1 << playerList);
-
-			delay(slowdown[a++]);
-		}
+		P0 = ledOn(1 << getRandomList());
+		
+		delay(DELAY_STD);	 //delay(2000)
 	}
+
+    while (slowdown[a]!= 0)
+	{
+		playerList = getRandomList();
+		P0 = ledOn(1 << playerList);
+
+		delay(slowdown[a]);
+		a++;
+	}
+
+	while (starPin != LOW);
+	
 } /* end stateList */
+
+void main(void)
+{
+//	for(;;)
+//	{
+		stateList();
+//	}
+} /* end main */
